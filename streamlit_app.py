fb61ea625954990706cddff25dcef07dcac09b2e
IbtesamSorab <101421734+IbtesamSorab@users.noreply.github.com>
2023-03-06 11:53:15 +0530

    formatted json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,11 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
+#streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
+
+# normalizes the json data received
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# converts data to a tabular form
+streamlit.dataframe(fruityvice_normalized)
