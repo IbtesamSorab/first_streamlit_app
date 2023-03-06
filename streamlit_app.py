838994ddffbbf04703b2e2cdeb4f916c617b50f0
IbtesamSorab <101421734+IbtesamSorab@users.noreply.github.com>
2023-03-06 14:47:06 +0530

    fetching all

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ my_cur = my_cnx.cursor()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains")
 streamlit.text(my_data_row)
 
